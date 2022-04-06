d8f9eb1de379f8f891bd042fc837acf132b90653
Михаил <@DESKTOP-3P2543V>
2022-04-06 20:48:10 +0300

    Добавьте файлы проекта.

---
+++ vASEK3-4/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace vASEK3_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Func = new System.Windows.Forms.TextBox();
+            this.Textxn = new System.Windows.Forms.TextBox();
+            this.Textx0 = new System.Windows.Forms.TextBox();
+            this.Vich = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Texth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.GetFile = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Func
+            // 
+            this.Func.Location = new System.Drawing.Point(198, 8);
+            this.Func.Multiline = true;
+            this.Func.Name = "Func";
+            this.Func.Size = new System.Drawing.Size(232, 368);
+            this.Func.TabIndex = 0;
+            // 
+            // Textxn
+            // 
+            this.Textxn.Location = new System.Drawing.Point(6, 37);
+            this.Textxn.Name = "Textxn";
+            this.Textxn.Size = new System.Drawing.Size(100, 23);
+            this.Textxn.TabIndex = 1;
+            // 
+            // Textx0
+            // 
+            this.Textx0.Location = new System.Drawing.Point(6, 8);
+            this.Textx0.Name = "Textx0";
+            this.Textx0.Size = new System.Drawing.Size(100, 23);
+            this.Textx0.TabIndex = 2;
+            // 
+            // Vich
+            // 
+            this.Vich.Location = new System.Drawing.Point(18, 95);
+            this.Vich.Name = "Vich";
+            this.Vich.Size = new System.Drawing.Size(75, 23);
+            this.Vich.TabIndex = 4;
+            this.Vich.Text = "Порешать";
+            this.Vich.UseVisualStyleBackColor = true;
+            this.Vich.Click += new System.EventHandler(this.Vich_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Конечный x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Начальный x";
+            // 
+            // Texth
+            // 
+            this.Texth.Location = new System.Drawing.Point(6, 66);
+            this.Texth.Name = "Texth";
+            this.Texth.Size = new System.Drawing.Size(100, 23);
+            this.Texth.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Шаг h";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(939, 435);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.Textx0);
+            this.tabPage1.Controls.Add(this.Func);
+            this.tabPage1.Controls.Add(this.Vich);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.Texth);
+            this.tabPage1.Controls.Add(this.Textxn);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(931, 407);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Lab3";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.GetFile);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(931, 407);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Lab4";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(193, 77);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(491, 262);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "InfoRebuild";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(193, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(491, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // GetFile
+            // 
+            this.GetFile.Location = new System.Drawing.Point(112, 48);
+            this.GetFile.Name = "GetFile";
+            this.GetFile.Size = new System.Drawing.Size(75, 23);
+            this.GetFile.TabIndex = 1;
+            this.GetFile.Text = "File";
+            this.GetFile.UseVisualStyleBackColor = true;
+            this.GetFile.Click += new System.EventHandler(this.GetFile_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Количество папок с цифрой";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Func;
+        private System.Windows.Forms.TextBox Textxn;
+        private System.Windows.Forms.TextBox Textx0;
+        private System.Windows.Forms.Button Vich;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Texth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button GetFile;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ vASEK3-4/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace vASEK3_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double F1(double x)
+        {
+            double A = Math.Log(1/x);
+            return A;
+        }
+        private double F2(double x)
+        {
+            double A = Math.Pow(Math.Cosh(Math.Log(x)),-1);
+            return A;
+        }
+        private double F3(double x)
+        {
+            double A = Math.Sin(Math.Sqrt(Math.Exp(5 / x)));
+            return A;
+        }
+        private double F4(double x)
+        {
+            int i = 0;
+            double A = 0;
+            do
+            {
+                i++;
+                A += (i * x * x - 1) * Math.Sqrt(i);
+
+            } while (i<1000000);
+            return A;
+        }
+        private void Vich_Click(object sender, EventArgs e)
+        {
+            Func.Text = "";
+            double x0 = Convert.ToDouble(Textx0.Text);
+            double xn = Convert.ToDouble(Textxn.Text);
+            double h = Convert.ToDouble(Texth.Text),x = x0;
+            double F = 0;
+            int ch = 0;
+            do
+            {
+                if (x < 0 || x == 0)
+                {
+                    Func.Text += x + ": " + "ОDZ ne podxodit x";
+                    Func.Text += "\r\n"; 
+                    ch++;
+                }
+                else
+                {
+                    F = F1(x) + F2(x) + F3(x) + F4(x);
+                    Func.Text += x + ": " + F;
+                    Func.Text += "\r\n";
+                    ch++;
+                }
+                x = x0 + ch * h ;
+            } while (x <= xn);
+            if (ch > 0)
+            {
+                MessageBox.Show( "Odno ili ne odno znach ne korect");
+            }
+        }
+
+        private void GetFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = textBox1.Text;
+            string[] Direct = path.Split(new char[] { '\\' });
+            string[] DirectDel = path.Split(new char[] { '\\' });
+            int n = 0;
+            char[] number = new char[10] { '0', '1', '2', '3', '4', '5', '6','7','8','9' };
+            int DirNumb = 0;
+            string CreateSubPath = "";
+            foreach (string s in Direct)
+            {
+                n++;
+            }
+            textBox2.Text += "Old path: " + path;
+            textBox2.Text += "\r\n";
+            for (int i = 1; i< n-1; i++)
+            {
+                int ch = -1;
+                ch = Direct[i].IndexOfAny(number);
+
+                if (ch != -1)
+                {
+                    DirNumb++;
+                    Direct[i] = Direct[i].Replace('0','A');
+                    Direct[i] = Direct[i].Replace('1', 'B');
+                    Direct[i] = Direct[i].Replace('2', 'C');
+                    Direct[i] = Direct[i].Replace('3', 'D');
+                    Direct[i] = Direct[i].Replace('4', 'E');
+                    Direct[i] = Direct[i].Replace('5', 'F');
+                    Direct[i] = Direct[i].Replace('6', 'G');
+                    Direct[i] = Direct[i].Replace('7', 'H');
+                    Direct[i] = Direct[i].Replace('8', 'I');
+                    Direct[i] = Direct[i].Replace('9', 'J');
+                }
+            }
+            string buff = Direct[1];
+            Direct[1] = Direct[n - 2];
+            Direct[n - 2] = buff;
+            for (int i = 1; i < n - 2; i++)
+            { 
+                    CreateSubPath += Direct[i] + "\\";
+            }
+            
+            CreateSubPath +=  Direct[n-2];
+            string CreatePath = Direct[0] +"\\"+ CreateSubPath;
+            for (int i = 3; i < n - 1; i++)
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(CreatePath);
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                }
+            }
+            try
+            {
+                File.Copy(textBox1.Text, CreatePath + "\\" + Path.GetFileName(textBox1.Text));
+            }
+            catch 
+            {
+                MessageBox.Show("Уже существует");
+            }
+            textBox2.Text += "New path: " + CreatePath + "\\" + Path.GetFileName(textBox1.Text);
+            textBox2.Text += "\r\n";
+            label4.Text = "Количество папок с цифрой: " + DirNumb;
+            string DirDel = DirectDel[0] + "\\" + DirectDel[1];
+            DirectoryInfo Info = new DirectoryInfo(DirDel);
+            if (Info.Exists)
+            {
+                Info.Delete(true);
+                Console.WriteLine("Каталог удален");
+            }
+            else
+            {
+                Console.WriteLine("Каталог не существует");
+            }
+        }
+    }
+}
